3678d6c126c1433956854d944c2573fd0e89c193
korchaginks <kkonstantinserg@gmail.com>
2023-07-22 20:22:03 +0300

    Написал sql код для создания таблиц через query tool

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,26 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE customers
+(
+	customer_id varchar(5) PRIMARY KEY NOT NULL,
+	company_name varchar(100) NOT NULL,
+	contact_name varchar(100) NOT NULL
+);
+
+CREATE TABLE employees
+(
+	employee_id int PRIMARY KEY NOT NULL,
+	first_name varchar (100) NOT NULL,
+	last_name varchar (100) NOT NULL,
+	title varchar (100) NOT NULL,
+	birth_date date NOT NULL,
+	notes text
+);
+
+CREATE TABLE orders
+(
+	order_id int PRIMARY KEY,
+	customer_id varchar(5) REFERENCES customers(customer_id) NOT NULL,
+	employee_id int REFERENCES employees(employee_id) NOT NULL,
+	order_date date NOT NULL,
+	ship_city varchar(100) NOT NULL
+);
