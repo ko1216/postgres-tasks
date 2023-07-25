ce2556c3affd9b941fe953fd23dce29ee82c4e6a
korchaginks <kkonstantinserg@gmail.com>
2023-07-25 22:37:16 +0300

    Написал 3/4 запросов по дз

--- homework-3/queries.sql
+++ homework-3/queries.sql
@@ -1,7 +1,12 @@
 -- Напишите запросы, которые выводят следующую информацию:
 -- 1. Название компании заказчика (company_name из табл. customers) и ФИО сотрудника, работающего над заказом этой компании (см таблицу employees),
 -- когда и заказчик и сотрудник зарегистрированы в городе London, а доставку заказа ведет компания United Package (company_name в табл shippers)
-
+SELECT customers.company_name, CONCAT(first_name, ' ', last_name) AS fullname
+FROM orders
+JOIN shippers ON orders.ship_via = shippers.shipper_id
+JOIN customers USING(customer_id)
+JOIN employees USING(employee_id)
+WHERE customers.city=employees.city AND employees.city IN ('London') AND shippers.company_name IN ('United Package');
 
 -- 2. Наименование продукта, количество товара (product_name и units_in_stock в табл products),
 -- имя поставщика и его телефон (contact_name и phone в табл suppliers) для таких продуктов,
@@ -10,7 +15,18 @@
 
 
 -- 3. Список компаний заказчиков (company_name из табл customers), не сделавших ни одного заказа
-
+SELECT company_name
+FROM customers
+WHERE customer_id NOT IN (SELECT customer_id FROM orders);
 
 -- 4. уникальные названия продуктов, которых заказано ровно 10 единиц (количество заказанных единиц см в колонке quantity табл order_details)
 -- Этот запрос написать именно с использованием подзапроса.
+SELECT DISTINCT product_name
+FROM products
+WHERE EXISTS (SELECT quantity FROM order_details WHERE quantity = 10 AND products.product_id=order_details.product_id)
+
+-- без подзапроса:
+SELECT DISTINCT product_name
+FROM products
+JOIN order_details USING(product_id)
+WHERE quantity = 10
